f1343b3bc1e02eec543586d1e1d7be7a4ff9d9fd
GaryBentham <Gary.Bentham@capp.co>
2016-03-16 14:22:27 +0000

    Added files via upload

---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using PerfSurf;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace PerfSurf
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
